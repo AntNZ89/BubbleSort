81703782931b9fb4b77d2ab4e69771bbba3f3d4f
Anton Example <youremail@example.com>
2025-08-02 21:20:08 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,58 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double[] array = new double[10];
+
+        for (int c = 0 ; c < array.length ; c++) {
+            array[c] = Math.random();
+        }
+
+        System.out.println(getArrayAsString(array));
+        double swapNum1;
+        double swapNum2;
+        int count;
+
+        while (true) {
+
+            count = 0;
+
+            for (int c = 0; c < array.length; c++) {
+
+                if (c < 9) {
+                    if (array[c] > array[c + 1]) {
+                        swapNum1 = array[c];
+                        swapNum2 = array[c + 1];
+                        array[c] = swapNum2;
+                        array[c + 1] = swapNum1;
+                    }
+                    else {
+                        count += 1;
+                    }
+                }
+            }
+            if (count == 9){
+                break;
+            }
+        }
+        System.out.println(getArrayAsString(array));
+    }
+
+
+    public static String getArrayAsString(double[] array){
+
+        String s = "[";
+
+        for (int c = 0 ; c < array.length ; c++){
+            if (c < 9){
+                s += array[c];
+                s += " , ";
+            }
+            else {
+                s += array[c];
+            }
+        }
+        s += "]";
+        return s;
+    }
+
+}
